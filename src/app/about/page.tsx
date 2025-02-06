287c50f785b428caa4c61985f32e5a8b658bca6a
eli <e.feasley@gmail.com>
2025-02-06 02:37:38 -0500

    Update the about text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,13 +8,14 @@ const AboutPage = () => {
             Welcome to Distribute Aid Needs Assessment!
           </h1>
           <p className="text-gray-700 text-lg leading-relaxed mb-6">
-            Distribute Aid is dedicated to assessing and addressing the needs of
-            communities worldwide. By completing surveys, you contribute valuable
-            insights that help optimize the distribution of essential resources
-            such as water, food, shelter, and hygiene supplies.
+          The results from this survey will allow <b><a href='https://distributeaid.org/'>Distribute Aid</a></b> to understand your region’s and organisation’s needs over the next six months so
+          we can advise collection groups on what to collect,
+          figure out which targeted campaigns to run,
+           and decide who to reach out to for in-kind donations.
+           In short, this assessment helps make sure the aid you receive is better suited to your needs.
           </p>
           <p className="text-gray-700 text-lg leading-relaxed mb-8">
-            If you would like an account to participate in our surveys, please
+            If you would like an account please
             contact us at{" "}
             <a
               href="mailto:hello@distributeaid.org"
